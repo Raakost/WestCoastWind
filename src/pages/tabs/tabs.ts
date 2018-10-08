770eed9ea9875b580a54b37cc330328636f4bf1f
Raakost <jeanettesydboege@gmail.com>
2018-10-08 10:48:50 +0200

    Removed unused pages and imports
    ..

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
 import {WindChartPage} from "../wind-chart/wind-chart";
-import {ForecastPage} from "../forecast/forecast";
 import {HistoryPage} from "../history/history";
 
 @Component({
@@ -11,7 +10,6 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = WindChartPage;
-  // tab3Root = ForecastPage;
   tab4Root = HistoryPage;
 
   constructor() {
